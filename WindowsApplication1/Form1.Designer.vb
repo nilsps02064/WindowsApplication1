6f7441ef9b8302520cb89384c4509fe945a3f08a
nilsps02064 <leesynj@yahoo.com.vn>
2015-04-13 17:17:13 +0700

    Demo
    add

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -30,9 +30,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(106, 43)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Lê So Ni"
+        Me.Label1.Text = "Lê So Ni PS02064"
         '
         'Form1
         '
